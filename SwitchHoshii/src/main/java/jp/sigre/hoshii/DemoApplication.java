48c964eec9e03f5aea785530769829d2d7af680c
sigre <sigre@DESKTOP-B4BKDDT>
2017-07-26 01:42:12 +0900

    Line デモアプリ追加

---
+++ SwitchHoshii/src/main/java/jp/sigre/hoshii/DemoApplication.java
@@ -0,0 +1,56 @@
+/**
+ *
+ */
+package jp.sigre.hoshii;
+
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.linecorp.bot.client.LineMessagingService;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+/**
+ * @author sigre
+ *
+ */
+
+
+@SpringBootApplication
+@LineMessageHandler //----- ココを追加
+public class DemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+
+//----- ここから -----
+  @Autowired
+  private LineMessagingService lineMessagingService;
+
+  @EventMapping
+  public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws Exception {
+    System.out.println("event: " + event);
+    final BotApiResponse apiResponse = lineMessagingService
+        .replyMessage(new ReplyMessage(event.getReplyToken(),
+                                       Collections.singletonList(new TextMessage(event.getSource().getUserId()))))
+        .execute().body();
+    System.out.println("Sent messages: " + apiResponse);
+  }
+
+  @EventMapping
+  public void defaultMessageEvent(Event event) {
+    System.out.println("event: " + event);
+  }
+//----- ここまで追加 -----
+
+}
