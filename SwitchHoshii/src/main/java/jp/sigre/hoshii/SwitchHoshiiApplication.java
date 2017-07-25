a25c3786c6074eb76d88a554e7c9496cb013d3cb
sigre <sigre@DESKTOP-B4BKDDT>
2017-07-26 01:11:17 +0900

    SpringBoot プロジェクト作成

---
+++ SwitchHoshii/src/main/java/jp/sigre/hoshii/SwitchHoshiiApplication.java
@@ -0,0 +1,12 @@
+package jp.sigre.hoshii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwitchHoshiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwitchHoshiiApplication.class, args);
+	}
+}
